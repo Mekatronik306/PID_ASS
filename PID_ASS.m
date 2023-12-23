59670de7d33b3ab0e4bd4dc78378a195521ec135
Pontus Svensson <pontus.github@doughnut.anonaddy.com>
2023-12-23 16:20:37 +0100

    only using velocity and displacement

--- PID_ASS.m
+++ PID_ASS.m
@@ -6,7 +6,8 @@ m_u = 50; % kg
 
 A = [0,1,0,0;-(k_s/m_s),-(b_s/m_s),k_s/m_s,b_s/m_s;0,0,0,1;k_s/m_u,b_s/m_u,-(k_t+k_s)/m_u,-(b_s/m_u)];
 B = [0,0;1/m_s,0;0,0;0,k_t/m_u];
-C = [-(k_s/m_s), -(b_s/m_s), k_s/m_s, b_s/m_s; 1,0,0,0; k_s/m_u,b_s/m_u, -(k_t+k_s)/m_u,-(b_s)/m_u;0,0,1,0];
+% C = [1,0,0,0;-(k_s/m_s), -(b_s/m_s), k_s/m_s, b_s/m_s;0,0,1,0;  k_s/m_u,b_s/m_u, -(k_t+k_s)/m_u,-(b_s)/m_u];
+C = [1,0,0,0;0,1,0,0;0,0,1,0;0,0,0,1;];
 D = [0,0;0,0;0,0;0,0];
 
 % Convert the state-space representation to transfer function
