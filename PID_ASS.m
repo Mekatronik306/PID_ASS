be16a0a407ed171d7cc220e9e2ede0a9993243a2
Pontus Svensson <pontus.github@doughnut.anonaddy.com>
2023-12-23 14:18:53 +0100

    variables changed

--- PID_ASS.m
+++ PID_ASS.m
@@ -6,7 +6,7 @@ m_u = 10;
 
 A = [0,1,0,0;-(k_s/m_s),-(b_s/m_s),k_s/m_s,b_s/m_s;0,0,0,1;k_s/m_u,b_s/m_u,-(k_t+k_s)/m_u,-(b_s/m_u)];
 B = [0,0;1/m_s,0;0,0;0,k_t/m_u];
-C = [-(k_s/m_s), -(b_s/m_s), k_s/m_s, b_s/m_s; 0,1,0,0; k_s/m_u,b_s/m_u, -(k_t+k_s)/m_u,-(b_s)/m_u;0,0,0,1];
+C = [-(k_s/m_s), -(b_s/m_s), k_s/m_s, b_s/m_s; 1,0,0,0; k_s/m_u,b_s/m_u, -(k_t+k_s)/m_u,-(b_s)/m_u;0,0,1,0];
 D = [0,0;0,0;0,0;0,0];
 
 % Convert the state-space representation to transfer function
